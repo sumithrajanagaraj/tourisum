34d27a68d406b3eea86af334386f02910d304cc5
sumithrajanagaraj <sumithrajanagarajm@gmail.com>
2020-04-28 10:43:27 +0530

    main page

---
+++ tourisum/main.php
@@ -0,0 +1,31 @@
+<html>
+<title>MyTraval</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+<head>
+<style>
+.back_image{background-repeat:no-repeat;background-size:cover; height:100%; width=100%;background-image: url("main_back.jpg");}
+body, html {height: 100%;}hidden;}
+ul{list-style-type:none; margin:0; padding:0;overflow:hidden;background-color:black;}
+li{float:left;list-style-type:none;border-right:1px solid white;color:white;font-size:150%;padding-right:20px;padding-left:20px;font-size:140%;}
+.img{width:10%;height:10%;}
+.heading{font-size:200%;color:black;background-color:white;}
+.li1{padding-right:20px;}
+</style>
+</head>
+<body>
+<div class="back_image">
+<br>
+<div class="heading">
+<p><img class="img" src="air.png"/>MyTraval</p>
+</div>
+<ul class="li">
+<li>91-9361473556</li>
+<li>mytraval@gmail.com &#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;</li>
+<li><img src="facebook.png"/></li>
+<li><img src="twitter.png"/></li>
+<li><img src="insta.png"/></li>
+<li><img src="signin.png"/>  Sign In or Register</li>
+</ul>
+</div>
+</body>
+</html>
